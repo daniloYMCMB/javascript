3625ac9161beb412ad7957f28a2b27c609c3d188
Danilo <danilojesusv@gmail.com>
2018-09-26 21:07:45 -0500

    variables, functions and objects

---
+++ objetos.js
@@ -0,0 +1,20 @@
+//Objetos
+var sacha = {
+	nombre: 'Danilo',
+	viacava: 'Vizarreta',
+	edad: 24,
+	trabajos: {
+		primero: 'idearte',
+		segundo: 'staff creativa',
+		tercero: 'staff digital',
+		cuarto: 'canalIPe',
+		quinto: 'mambo'
+	}
+}
+
+function nombreMayus(nombre) {
+	nombre = nombre.toUpperCase();
+	console.log(nombre)
+}
+
+nombreMayus(Danilo)
